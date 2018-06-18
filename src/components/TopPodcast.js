89a6b4e1b5b7c348b3cabe65789255d34e13a74a
AllisonBasore <allison.basore@students.olin.edu>
2018-06-18 18:17:55 -0400

    put back iamge blocks

--- src/components/TopPodcast.js
+++ src/components/TopPodcast.js
@@ -1,7 +1,7 @@
 
 
 import React from 'react'
-//import ReactImageFallback from "react-image-fallback";
+import ReactImageFallback from "react-image-fallback";
 import PropTypes from 'prop-types'
 require('../stylesheets/main.css')
 
@@ -14,15 +14,15 @@ const TopPodcast = ({podcast, onClick}) => (
 			<div className="TopPodcast_Indi_Box">
 
 				<div>
-	          //         <div className="Image_Box">
-			        //     // <ReactImageFallback
-			        //     //     src={podcast.logo_url}
-			        //     //     fallbackImage="https://i0.wp.com/jordanbpeterson.com/wp-content/uploads/2016/12/Podcast-Icon-v3.png?resize=229%2C300&ssl=1"
-			        //     //     initialImage={podcast.logo_url}
-			        //     //     alt="Podcast Image"
-			        //     //     className="Podcast_Image"
-			        //     // />
-			        // </div>
+	                  <div className="Image_Box">
+			            <ReactImageFallback
+			                src={podcast.logo_url}
+			                fallbackImage="https://i0.wp.com/jordanbpeterson.com/wp-content/uploads/2016/12/Podcast-Icon-v3.png?resize=229%2C300&ssl=1"
+			                initialImage={podcast.logo_url}
+			                alt="Podcast Image"
+			                className="Podcast_Image"
+			            />
+			        </div>
 	            </div>
 		        <p className="TopPodcast_Main">
 					<div>
