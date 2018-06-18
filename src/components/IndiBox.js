1e2709adbaffb7692ab606ffb2dc2b693a146eab
AllisonBasore <allison.basore@students.olin.edu>
2018-06-18 13:16:43 -0400

    temp took out img

--- src/components/IndiBox.js
+++ src/components/IndiBox.js
@@ -1,7 +1,7 @@
 
 import React from 'react'
 import PropTypes from 'prop-types'
-import ReactImageFallback from "react-image-fallback";
+//import ReactImageFallback from "react-image-fallback";
 
 
 const IndiBox = ({podcast, onClick}) => (
@@ -11,13 +11,13 @@ const IndiBox = ({podcast, onClick}) => (
 			<i class="fa fa-times" aria-hidden="true"></i>
 		</button>
 		<div className="Image">
-            <ReactImageFallback
-                src={podcast.logo_url}
-                fallbackImage="https://i0.wp.com/jordanbpeterson.com/wp-content/uploads/2016/12/Podcast-Icon-v3.png?resize=229%2C300&ssl=1"
-                initialImage={podcast.logo_url}
-                alt="Podcast Image"
-                className="Podcast_Image"
-            />
+            // <ReactImageFallback
+            //     src={podcast.logo_url}
+            //     fallbackImage="https://i0.wp.com/jordanbpeterson.com/wp-content/uploads/2016/12/Podcast-Icon-v3.png?resize=229%2C300&ssl=1"
+            //     initialImage={podcast.logo_url}
+            //     alt="Podcast Image"
+            //     className="Podcast_Image"
+            // />
 		</div>
 		<div className="Text">
 			{podcast.description}
